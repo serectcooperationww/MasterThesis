b3382958456139d1df48c368f5efea96fcda22cc
ZhiziWen <wenzhizi@foxmail.com>
2024-01-15 00:32:33 +0100

    add resampling method

--- code/resampling_and_classification.py
+++ code/resampling_and_classification.py
@@ -1,17 +1,24 @@
-from imblearn.under_sampling import RandomUnderSampler
-from sklearn.preprocessing import LabelEncoder, StandardScaler
-from sklearn.model_selection import StratifiedKFold
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report
-from imblearn.over_sampling import RandomOverSampler, SMOTE, ADASYN
-from imblearn.under_sampling import RandomUnderSampler, NearMiss
-from LSTMencoder_pytorch import LSTM, SequenceDataset
+from imblearn.over_sampling import RandomOverSampler, SMOTE, BorderlineSMOTE, ADASYN
+from imblearn.under_sampling import RandomUnderSampler, AllKNN, CondensedNearestNeighbour, EditedNearestNeighbours, RepeatedEditedNearestNeighbours, TomekLinks, InstanceHardnessThreshold, NearMiss, OneSidedSelection
+from imblearn.combine import SMOTEENN, SMOTETomek
+from imblearn.under_sampling import NeighbourhoodCleaningRule
 
 
 resampling_techniques = {
-        "Random Over-Sampling": RandomOverSampler(random_state=0),
-        "Random Under-Sampling": RandomUnderSampler(random_state=0),
-        "SMOTE": SMOTE(random_state=0),
+        "Random Over-Sampling (RO)": RandomOverSampler(random_state=0),
+        "SMOTE Over-Sampling (SM)": SMOTE(random_state=0),
+        "BorderlineSMOTE Over-Sampling (BS)": BorderlineSMOTE(random_state=0),
+        "Random Under-Sampling (RU)": RandomUnderSampler(random_state=0),
+        "All k-nearest neighbours Under-Sampling (AL)": AllKNN(),
+        "Condensed nearest neighbours Under-Sampling (CN)": CondensedNearestNeighbour(random_state=0),
+        "Edited nearest neighbours Under-Sampling (EN)": EditedNearestNeighbours(),
+        "Repeated edited nearest neighbours Under-Sampling": RepeatedEditedNearestNeighbours(),
+        "Synthetic minority oversampling-Edited nearest neighbour Hybrid-Sampling (SE)": SMOTEENN(random_state=0),
+        "Tomek links Under-Sampling(TM)": TomekLinks(),
+        "Synthetic minority oversampling-Tomek’s link Over-Sampling (ST)": SMOTETomek(),
+        "Instance hardness threshold Under-Sampling (IH)": InstanceHardnessThreshold(),
+        "Neighbourhood cleaning rule Hybrid-Sampling(NC)": NeighbourhoodCleaningRule(),
+        "One-sided selection (OS)": OneSidedSelection(),
         "ADASYN": ADASYN(random_state=0),
         "NearMiss": NearMiss(version=1),
         "No Filter": None
